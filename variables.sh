cab38448295deee9b7d9033a5abf932aecf7a24a
Ken Collins <ken@metaskills.net>
2024-07-04 11:27:11 -0400

    Refactor variables.sh.

--- variables.sh
+++ variables.sh
@@ -1,6 +1,8 @@
+# llamafile=Mistral-7B-Instruct-v0.3.Q4_0.llamafile
+# llamafileurl="https://huggingface.co/Mozilla/Mistral-7B-Instruct-v0.3-llamafile/resolve/main/Mistral-7B-Instruct-v0.3.Q4_0.llamafile"
 
-llamarepo="Phi-3-mini-4k-instruct-llamafile"
-llamafile="Phi-3-mini-4k-instruct.F16.llamafile"
+llamafile=Phi-3-mini-4k-instruct.Q2_K.llamafile
+llamafileurl="https://huggingface.co/Mozilla/Phi-3-mini-4k-instruct-llamafile/resolve/main/Phi-3-mini-4k-instruct.Q2_K.llamafile"
 
-llamaimage="llamafile-on-lambda"
+llamafileimage="llamafile-on-lambda"
 llamafiledir="./tmp/llamafile"
